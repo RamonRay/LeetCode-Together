eb1df3130c68b53a30e26d127e43984523f97472
Ramonray <Ramonray@pku.edu.cn>
2019-03-16 00:34:22 +0800

    147. Insertion Sort List.py

---
+++ Hansome Ray/147. Insertion Sort List.py
@@ -0,0 +1,43 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+def insertionSortList(head):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
+    p=dummy=ListNode(0)
+    cur=dummy.next=head
+    while cur and cur.next:
+        nextVal=cur.next.val
+        if nextVal>cur.val:
+            cur=cur.next
+            continue
+        if p.next.val>nextVal:
+            p=dummy
+        while p.next.val<nextVal:
+            p=p.next
+        tmp=cur.next
+        cur.next=tmp.next
+        tmp.next=p.next
+        p.next=tmp
+    return dummy.next
+def init(ans):
+    head=ListNode(ans[0])
+    tmp=head
+    for i in range(1,len(ans)):
+        new=ListNode(ans[i])
+        tmp.next=new
+        tmp=new
+    return head
+def read(head):
+   print head.val
+   head=head.next
+   while head!=None:
+      print head.val
+      head=head.next
+head=init([4,2,1,3])
+head=insertionSortList(head)
+read(head)
+
